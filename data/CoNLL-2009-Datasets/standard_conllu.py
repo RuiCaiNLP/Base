7ea1ce8d25f6729a6d0f386bc5ff8f3329a4abc4
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2019-09-08 19:41:59 +0100

    fr

---
+++ data/CoNLL-2009-Datasets/standard_conllu.py
@@ -0,0 +1,19 @@
+file_in = open("fr-up-test.conllu", 'r')
+file_out = open("fr-test", 'w')
+
+start = True
+
+for line in file_in.readlines():
+    part = line.strip().split()
+    if len(part)<=1:
+        file_out.write('\n')
+        start = True
+        continue
+    if start:
+        start = False
+        continue
+    new_parts = [part[0], part[1],  '_', part[2], part[3],  part[4], '_',  '_', '_', part[6], '_' ]
+    new_parts = new_parts + part[7:]
+    line = '\t'.join(new_parts)
+    file_out.write(line)
+    file_out.write('\n')
