abc05da9e1be17a62fefb563247a8455f930c2e2
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2019-10-08 22:04:15 +0100

    adversarial

--- Adversarial_model.py
+++ Adversarial_model.py
@@ -268,12 +268,12 @@ class Adversarial_TModel(nn.Module):
             prob_real_decision = self.Discriminator(real_states.detach())
             prob_fake_decision = self.Discriminator(fake_states.detach())
             D_loss= - torch.mean(torch.log(prob_real_decision) + torch.log(1. - prob_fake_decision))
-            log("D loss:", D_loss)
+            #log("D loss:", D_loss)
             return D_loss
         else:
             prob_fake_decision_G = self.Discriminator(fake_states)
             G_loss = -torch.mean(torch.log(prob_fake_decision_G))
-            log("G loss:", G_loss)
+            #log("G loss:", G_loss)
             return G_loss
 
 
