6e9f86ee5c0bbd275b908f07d28b348a4417ff9c
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2019-10-09 10:34:42 +0100

    shuffle

--- Adversarial_model.py
+++ Adversarial_model.py
@@ -89,6 +89,9 @@ class EN_Labeler(nn.Module):
         cat_output = en_output.view(self.batch_size, seq_len, -1)
         pred_recur = pred_recur.unsqueeze(1).expand(self.batch_size, seq_len, 2*self.bilstm_hidden_size)
         all_cat = torch.cat((hidden_input, pred_recur, cat_output), 2)
+        shuffled_timestep = np.arange(0, seq_len)
+        np.random.shuffle(shuffled_timestep)
+        all_cat = all_cat.index_select(dim=1, index=get_torch_variable_from_np(shuffled_timestep))
 
         return en_output, all_cat
 
