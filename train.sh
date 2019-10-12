e31689df5331e20d8c4ffa307c715a625324194b
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2019-10-12 16:27:28 +0100

    fr

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@ python run_GAN.py  --train --train_data data/En_train.dataset --valid_data data/
  --seed 100 --tmp_path temp --model_path model --result_path result --pretrain_embedding data/en.vec.txt\
   --pretrain_emb_size 300  --epochs 300 --dropout 0.1 --lr 0.001 --batch_size 1 \
   --word_emb_size 300 --pos_emb_size 32 --lemma_emb_size 100 --use_deprel --deprel_emb_size 64 \
-  --bilstm_hidden_size 20 --bilstm_num_layers 2 \
+  --bilstm_hidden_size 400 --bilstm_num_layers 3 \
   --valid_step 1000  --use_flag_emb --flag_emb_size 16 --clip 5 \
   --dropout_word 0.3 --dropout_mlp 0.5 --use_biaffine #--preprocess
 #--use_highway --highway_num_layers 10  --preprocess
